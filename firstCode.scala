b75e915ad10460484db17fc3e2ab08bb638a098c
sellgithubfor9b <40630212+sellgithubfor9b@users.noreply.github.com>
2021-07-31 20:20:11 +0530

    big-data-training

---
+++ firstCode.scala
@@ -0,0 +1,23 @@
+package com.spark.sr
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+import org.apache.log4j._
+
+
+
+
+object secondCode {
+  
+  def main(args:Array[String]) = {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    val sc = new SparkContext("local[*]", "firstProgram")
+    
+    
+    val rdd = sc.textFile("C:/Users/user/OneDrive/windows/del.txt")
+    val count = rdd.count()
+    println("Count of words is: " + count)
+
+    
+  }
+  
+}
